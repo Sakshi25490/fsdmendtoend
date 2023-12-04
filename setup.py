e69acc60892830f818c177e849b08924a65958cc
“Sakshi <sakshihiremaths@gmail.com>
2023-12-04 17:36:57 +0530

    updated all the changes

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='Sakshi Hiremath',
+    author_email='sakshihiremaths@gmail.com',
+    packages=find_packages()
+)
